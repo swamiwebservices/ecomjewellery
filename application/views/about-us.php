2481b2cf2a90d863eed56eba57c7b66ea7454ddb
kishore mishra <swamiwebservices@gmail.com>
2022-11-13 19:54:59 +0530

    new info pages

---
+++ application/views/about-us.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <?php $this->load->view('inc_metacss'); ?>
+    <style>
+     
+        
+    </style>
+</head>
+
+<body>
+    <div class="home_black_version">
+
+        <?php $this->load->view('inc_header_menu'); ?>
+
+
+       <!--breadcrumbs area start-->
+    <div class="breadcrumbs_area">
+        <div class="container">   
+            <div class="row">
+                <div class="col-12">
+                    <div class="breadcrumb_content">
+                        <h3>about us</h3>
+                        <ul>
+                        <li><a href="<?php echo site_url('home')?>">Home</a></li>
+                                <li>&gt;</li>
+                          
+                            <li>about us</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>         
+    </div>
+    <!--breadcrumbs area end-->
+    <div class="unlimited_services">
+        <div class="container">  
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-12">
+                    <div class="services_section_thumb">
+                        <img src="<?php echo base_url();?>assets/img/aboutus.jpg" style="border-radius:35px" alt="aboutus">
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <div class="unlimited_services_content">
+                        <h1>Welcome Bond-Beyond</h1>
+                        <p>Bond Beyond	is a luxury silver collection with fine designs and assured quality is  last elegance beyond generations.
+                        </p>
+                        <p>
+Silver is rare,	limited	and the investment	value it carries	drives	the importance from ages.	The Unique	quality,	elegance , and	affordability	makes	it fall	in	love for	any generation.
+</p>
+<p>
+Bond	Beyond iS a one stop destination for silver collections from traditional to modern,
+budgeted	to Customized	articles	with certified quality	to add value now and beyond.
+It	offers	wine	range	of	silverwares	such	as	plates,	bowls,	spoons,	forks,	gloss,
+exquisite center pieces, tableware, photo frames, coins, jewelry, decorative items and
+many other gift articles	which are Unique, elegant and unparallel with a high quality and assured returns.
+ </p>
+ 
+
+       
+
+<h2>All type of Pooja articles – Pooja Thali, Tamhan, Diya, Kalash, Lota,
+Ghanti, etc.</h2>
+<ul>
+<li ><i class="fa fa-snowflake-o" aria-hidden="true"></i> All type of God idols</li>
+<li ><i class="fa fa-snowflake-o" aria-hidden="true"></i> Fancy gift articles from AED 50 and above</li>
+ <li ><i class="fa fa-snowflake-o" aria-hidden="true"></i> All types of Plates, Glasses, Bowls, Spoons, Forks, etc.</li>
+ <li ><i class="fa fa-snowflake-o" aria-hidden="true"></i> Variety of Coins</li>
+ <li ><i class="fa fa-snowflake-o" aria-hidden="true"></i> 24 Karat Gold Chain / Bangles / Kada</li>
+ <li ><i class="fa fa-snowflake-o" aria-hidden="true"></i> Authentic Pearls Jewellery</li>
+    </ul>
+ 
+                        <!-- <div class="view__work">
+                            <a href="#">MORE INFO  <i class="fa fa-angle-right"></i></a>
+                        </div>   -->
+                    </div>
+                </div>
+            </div>
+        </div>     
+    </div>
+     <!--about section area -->
+     
+    <!--about section end-->
+            
+    <!--chose us area start-->
+    <div class="choseus_area mt-5">
+        <div class="container">
+            <div class="row">
+               <div class="col-12">
+                   <div class="chose_title">
+                       <h1>Why chose us?</h1>
+                   </div>
+               </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single_chose">
+                        <div class="chose_icone">
+                            <img src="assets/img/about/About_icon1.jpg" alt="">
+                        </div>
+                        <div class="chose_content">
+                            <h3>Creative Design</h3>
+                            <!-- <p>Erat metus sodales eget dolor consectetuer, porta ut purus at et alias, nulla ornare velit amet enim</p> -->
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single_chose">
+                        <div class="chose_icone">
+                            <img src="assets/img/about/About_icon2.jpg" alt="">
+                        </div>
+                        <div class="chose_content">
+                            <h3>100% Money Back Guarantee</h3>
+                            <!-- <p>Erat metus sodales eget dolor consectetuer, porta ut purus at et alias, nulla ornare velit amet enim</p> -->
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single_chose">
+                        <div class="chose_icone">
+                            <img src="assets/img/about/About_icon3.jpg" alt="">
+                        </div>
+                        <div class="chose_content">
+                            <h3>Online Support 24/7</h3>
+                            <!-- <p>Erat metus sodales eget dolor consectetuer, porta ut purus at et alias, nulla ornare velit amet enim</p> -->
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--chose us area end-->
+ 
+
+        <?php $this->load->view('inc_footer'); ?>
+
+        <!-- JS ============================================ -->
+        <?php $this->load->view('inc_common_footer_js'); ?>
+
+
+</body>
+
+</html>
