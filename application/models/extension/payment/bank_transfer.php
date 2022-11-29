f85af7af09fab0c087092ff5daa1d58baf3a4bdc
kishore mishra <swamiwebservices@gmail.com>
2022-11-29 09:55:24 +0530

    none

--- application/models/extension/payment/bank_transfer.php
+++ application/models/extension/payment/bank_transfer.php
@@ -2,7 +2,6 @@
 	class Bank_transfer extends CI_Model {
 	public function getMethod($address, $total) {
 	 
-	 
 		$method_data = array(
 			'code'       => 'bank_transfer',
 			'title'      => 'Bank Transfer',
